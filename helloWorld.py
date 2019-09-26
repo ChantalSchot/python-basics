15a722674dea81edce891914de40df64a0d5664b
Chantal <chantalschot@outlook.com>
2019-09-26 14:04:21 +0200

    Basics of python (section 1-3, until lesson 11)

---
+++ helloWorld.py
@@ -0,0 +1,40 @@
+"""Variables"""
+
+intVal = 7
+floatVal = 3.91
+boolVal = True
+#print(boolVal)
+
+boolVal = False
+#print(intVal, floatVal, boolVal)
+
+"""testing comments"""
+
+"""So multiple line comments
+need 3 quotation marks"""
+
+"""Math operators:"""
+
+operator1 = 2
+operator1 //= 0.5
+#print (operator1)
+
+"""#Strings and Print"""
+
+# print("test")
+# print("this should be \"in quotes\"")
+var = "Alligator"
+# print(var[5])
+# print(var[:4])
+# print(var[4:7])
+# print(var[6:])
+""" len(varName) Gives length of variable:"""
+# print(len(var))
+# print(var.upper())
+"""To convert any variable to a string: use varName(num)"""
+# print(len("Manchester United"))
+int = 12345
+int = str(int)
+# print(int[2])
+# print("albania".upper())
+# print("BELGIUM".lower())
